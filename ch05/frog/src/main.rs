7cb5b0b9eb3d23c48f78a56bb91b56eed3ee3dff
takayuki murata <muratata@scala-com.jp>
2025-11-19 18:34:19 +0900

    use chmin

--- ch05/frog/src/main.rs
+++ ch05/frog/src/main.rs
@@ -4,23 +4,31 @@ fn main() {
     println!("Total steps: {}", total_steps);
 }
 
+fn chmin<T: PartialOrd + Copy>(vs: &mut Vec<T>, i: usize, x: T) {
+    if vs[i] > x {
+        vs[i] = x;
+    }
+}
+
 fn calculate_total_steps(steps: &Vec<i32>) -> i32 {
     let len = steps.len();
     if len == 0 || len == 1 {
         return 0;
     }
-    let mut dp = vec![0; len];
+    let mut dp = vec![i32::MAX; len];
+    dp[0] = 0;
     dp[1] = (steps[1] - steps[0]).abs();
     if len == 2 {
         return dp[1];
     }
 
     for i in 2..len {
-        // 飛飛ばして進む場合
-        let s1 = dp[i - 2] + (steps[i] - steps[i - 2]).abs();
-        // 1歩ずつ進む場合
-        let s2 = dp[i - 1] + (steps[i] - steps[i - 1]).abs();
-        dp[i] = s1.min(s2);
+        // 飛び飛ばして進む合
+        let v1 = dp[i - 2] + (steps[i] - steps[i - 2]).abs();
+        chmin(&mut dp, i, v1);
+
+        let v2 = dp[i - 1] + (steps[i] - steps[i - 1]).abs();
+        chmin(&mut dp, i, v2);
     }
     dp[len - 1]
 }
