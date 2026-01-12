4eb160874c48e88324f68feda98bc24fac08b16f
takayuki murata <muratata@scala-com.jp>
2026-01-12 21:35:13 +0900

    Update algorithm

--- ch06/ch6-3/src/main.rs
+++ ch06/ch6-3/src/main.rs
@@ -35,6 +35,39 @@ fn find(vs: &[usize], target: usize) -> Vec<usize> {
     ns
 }
 
+fn resolve2(vs: &[usize], m: usize) -> usize {
+    let n = vs.len();
+    let mut sums = Vec::with_capacity(n * (n + 1) / 2);
+
+    // 1 or 2つの要素の和で作れる数値を列挙
+    for i in 0..n {
+        sums.push(vs[i]);
+        for j in i..n {
+            sums.push(vs[i] + vs[j]);
+        }
+    }
+    sums.push(0);
+    sums.sort_unstable();
+
+    let mut ans = 0;
+
+    // 1 or 2つの要素の和の組み合わせ、つまり1,2,3,4つの要素の和で作れる数値を調べる
+    for &x in &sums {
+        if x > m {
+            break;
+        }
+        let rest = m - x;
+        let idx = sums.upper_bound(&rest);
+        if idx == 0 {
+            continue;
+        }
+        let y = sums[idx - 1];
+        ans = ans.max(x + y);
+    }
+
+    ans
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -51,11 +84,20 @@ mod tests {
     fn test_resolve() {
         let vs = vec![3, 14, 15, 9];
         assert_eq!(resolve(&vs, 50), 48);
+        assert_eq!(resolve2(&vs, 50), 48);
     }
 
     #[test]
     fn test_resolve2() {
         let vs = vec![16, 11, 2];
         assert_eq!(resolve(&vs, 21), 20);
+        assert_eq!(resolve2(&vs, 21), 20);
+    }
+
+    #[test]
+    fn test_resolve3() {
+        let vs = vec![16, 11, 9];
+        assert_eq!(resolve(&vs, 17), 16);
+        assert_eq!(resolve2(&vs, 17), 16);
     }
 }
